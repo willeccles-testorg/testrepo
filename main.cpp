f717e50a315a704d93f51cb433de3a2e719ad6fd
Will Eccles <will@eccles.dev>
2022-06-22 09:50:27 -0400

    some stuff

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-  std::cout << "Hello, World!\n";
+  const void* x = "Hello, World!\n";
+  const char* msg = reinterpret_cast<const char*>(x);
+  std::cout << msg;
   return 0;
 }
